5595e7c1e4c7efd34d6f9160cc299593897a229a
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2024-03-18 12:34:54 +0400

    Modify lat, lon,alt columns in sites table

---
+++ init/050-update-lat-lon-alt-sites.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `arbimon2`.`sites` MODIFY `lat` double default NULL;
+ALTER TABLE `arbimon2`.`sites` MODIFY `lon` double default NULL;
+ALTER TABLE `arbimon2`.`sites` MODIFY `alt` double default NULL;
