024c3a476ed1ff9f5dc426b39977c6122eeded77
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2024-06-20 16:06:14 +0400

    Add queries for tables updates

--- init/052-drop-not-used-table.sql
+++ init/052-drop-not-used-table.sql
@@ -1,4 +1,11 @@
-DROP TABLE IF EXISTS `sessions`;
-DROP TABLE IF EXISTS `site_log_files`;
-DROP TABLE IF EXISTS `site_data_log`;
-DROP TABLE IF EXISTS `site_event_log`;
+DROP TABLE IF EXISTS sessions;
+DROP TABLE IF EXISTS site_log_files;
+DROP TABLE IF EXISTS site_data_log;
+DROP TABLE IF EXISTS site_event_log;
+
+
+ALTER TABLE projects
+DROP description, DROP project_type_id, DROP is_enabled, DROP current_plan,
+DROP storage_usage, DROP processing_usage, DROP pattern_matching_enabled,
+DROP cnn_enabled, DROP aed_enabled, DROP clustering_enabled,
+DROP featured, COLUMN image, DROP reports_enabled;
