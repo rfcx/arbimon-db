ae59558b0808ec5956b440185bc3753ee8e3efb7
TooSeriuz <frongs@rfcx.org>
2024-10-02 22:09:40 +0700

    Add migration for dropping `site_log` tables

---
+++ init/058-drop-site-logs-table.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `arbimon2`.`site_log_files`;
+DROP TABLE IF EXISTS `arbimon2`.`site_event_log`;
+DROP TABLE IF EXISTS `arbimon2`.`site_data_log`;
+DROP TABLE IF EXISTS `arbimon2`.`site_data_log_health_types`;
+DROP TABLE IF EXISTS `arbimon2`.`site_data_log_plug_types`;
+DROP TABLE IF EXISTS `arbimon2`.`site_data_log_tech_types`;
+DROP TABLE IF EXISTS `arbimon2`.`site_connection_log`;
+DROP TABLE IF EXISTS `arbimon2`.`site_connection_types`;
+DROP TABLE IF EXISTS `arbimon2`.`site_aggrs`;
