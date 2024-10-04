7b1a11013db62da1df9ba86f41bc2152810afecc
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2024-10-04 15:32:41 +0400

    #11 update migration file

--- init/056-add-uri-param-to-aed-clustering.sql
+++ init/056-add-uri-param-to-aed-clustering.sql
@@ -4,10 +4,7 @@ ALTER TABLE `arbimon2`.`audio_event_detections_clustering`
 ADD COLUMN `uri_param` int DEFAULT NULL;
 
 UPDATE `arbimon2`.`audio_event_detections_clustering`
-SET uri_image = SELECT substring_index(substring_index(uri_image, '/', -1), '.', 1);
-
-UPDATE `arbimon2`.`audio_event_detections_clustering`
-SET uri_param = uri_image;
+SET uri_param = SELECT substring_index(substring_index(uri_image, '/', -1), '.', 1);
 
 ALTER TABLE `arbimon2`.`audio_event_detections_clustering`
 DROP uri_image;
