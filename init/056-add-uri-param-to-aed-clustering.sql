979fa5c254efa081151d146e20a9259367c2ed22
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2024-10-16 16:50:42 +0400

    #11 Conver value to number

--- init/056-add-uri-param-to-aed-clustering.sql
+++ init/056-add-uri-param-to-aed-clustering.sql
@@ -4,7 +4,7 @@ ALTER TABLE `arbimon2`.`audio_event_detections_clustering`
 ADD COLUMN `uri_param` int DEFAULT NULL;
 
 UPDATE `arbimon2`.`audio_event_detections_clustering`
-SET uri_param = SELECT substring_index(substring_index(uri_image, '/', -1), '.', 1);
+SET uri_param = CAST((SELECT substring_index(substring_index(uri_image, '/', -1), '.', 1)) AS SIGNED);
 
 ALTER TABLE `arbimon2`.`audio_event_detections_clustering`
 DROP uri_image;
