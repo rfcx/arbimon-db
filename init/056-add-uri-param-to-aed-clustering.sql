e54f97d72a10cbef874af9b018cedb511fbafa0c
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2024-10-16 17:05:38 +0400

    #11 Change uri param type

--- init/056-add-uri-param-to-aed-clustering.sql
+++ init/056-add-uri-param-to-aed-clustering.sql
@@ -1,10 +1,10 @@
 -- uri_image = 'audio_events/prod/detection/33604/png/3450166/3.png'
 
 ALTER TABLE `arbimon2`.`audio_event_detections_clustering`
-ADD COLUMN `uri_param` int DEFAULT NULL;
+ADD COLUMN `uri_param` unsigned DEFAULT NULL;
 
 UPDATE `arbimon2`.`audio_event_detections_clustering`
-SET uri_param = CAST((SELECT substring_index(substring_index(uri_image, '/', -1), '.', 1)) AS SIGNED);
+SET uri_param = CAST((SELECT substring_index(substring_index(uri_image, '/', -1), '.', 1)) AS UNSIGNED);
 
 ALTER TABLE `arbimon2`.`audio_event_detections_clustering`
 DROP uri_image;
