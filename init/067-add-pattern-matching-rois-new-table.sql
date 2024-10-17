1158ea102c1639a14b240aec5c7b3826e61e8607
TooSeriuz <frongs@rfcx.org>
2024-10-17 14:35:21 +0700

    change to partition table

--- init/067-add-pattern-matching-rois-new-table.sql
+++ init/067-add-pattern-matching-rois-new-table.sql
@@ -1,9 +1,9 @@
 create table arbimon2.pattern_matching_rois_new (
-    pattern_matching_roi_id   int unsigned auto_increment primary key,
-    pattern_matching_id       int not null,
-    recording_id              bigint unsigned not null,
-    species_id                int not null,
-    songtype_id               int not null,
+    pattern_matching_roi_id   int unsigned auto_increment,
+    pattern_matching_id       int not null references arbimon2.pattern_matchings (pattern_matching_id),
+    recording_id              bigint unsigned not null references arbimon2.recordings (recording_id) on delete cascade,
+    species_id                int not null references arbimon2.species (species_id),
+    songtype_id               int not null references arbimon2.songtypes (songtype_id),
     x1                        float not null,
     y1                        mediumint unsigned not null,
     x2                        float not null,
@@ -17,26 +17,14 @@ create table arbimon2.pattern_matching_rois_new (
     consensus_validated       tinyint(1) null,
     expert_validated          tinyint(1) null,
     expert_validation_user_id smallint unsigned null,
-    denorm_site_id            int unsigned null,
+    denorm_site_id            int unsigned null references arbimon2.sites (site_id),
     denorm_recording_datetime datetime null,
     denorm_recording_date     date null,
-    KEY `fk_pattern_matching_matches_1_idx` (`pattern_matching_id`),
-    KEY `fk_pattern_matching_matches_2_idx` (`recording_id`),
-    KEY `fk_pattern_matching_matches_3_idx` (`species_id`),
-    KEY `fk_pattern_matching_matches_4_idx` (`songtype_id`),
-    KEY `fk_pattern_matching_rois_1_idx` (`denorm_site_id`),
+    PRIMARY KEY (pattern_matching_roi_id, pattern_matching_id),
     KEY `pattern_matching_matches_recording_score_idx` (`recording_id`,`score`),
-    KEY `pattern_matching_matches_site_score_idx` (`pattern_matching_id`,`denorm_site_id`,`score`),
-    KEY `pattern_matching_matches_site_datetime_score_idx` (`pattern_matching_id`,`denorm_site_id`,`denorm_recording_date`,`score`),
+    KEY `pattern_matching_matches_site_score_idx` (`denorm_site_id`,`score`),
+    KEY `pattern_matching_matches_site_datetime_score_idx` (`denorm_site_id`,`denorm_recording_date`,`score`),
     KEY `validated_idx` (`validated`),
-    constraint fk_pattern_matching_rois_2
-        foreign key (pattern_matching_id) references arbimon2.pattern_matchings (pattern_matching_id),
-    constraint fk_pattern_matching_rois_3
-        foreign key (recording_id) references arbimon2.recordings (recording_id) on delete cascade,
-    constraint fk_pattern_matching_rois_4
-        foreign key (species_id) references arbimon2.species (species_id),
-    constraint fk_pattern_matching_rois_5
-        foreign key (songtype_id) references arbimon2.songtypes (songtype_id),
-    constraint fk_pattern_matching_rois_6
-        foreign key (denorm_site_id) references arbimon2.sites (site_id)
-);
+    KEY `pattern_matching_id_idx` (`pattern_matching_id`),
+    KEY `denorm_site_id_idx` (`denorm_site_id`)
+) PARTITION BY HASH(pattern_matching_id) PARTITIONS 100;
