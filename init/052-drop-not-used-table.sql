409c8d05f9e94cb9897d1d45e6fd6a8b3057d369
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2024-10-30 20:08:07 +0400

    Fix 052-drop-not-used-table.sql migration

--- init/052-drop-not-used-table.sql
+++ init/052-drop-not-used-table.sql
@@ -1,11 +1,15 @@
 DROP TABLE IF EXISTS sessions;
-DROP TABLE IF EXISTS site_log_files;
-DROP TABLE IF EXISTS site_data_log;
-DROP TABLE IF EXISTS site_event_log;
 
-
-ALTER TABLE projects
-DROP description, DROP project_type_id, DROP is_enabled, DROP current_plan,
-DROP storage_usage, DROP processing_usage, DROP pattern_matching_enabled,
-DROP cnn_enabled, DROP aed_enabled, DROP clustering_enabled,
-DROP featured, COLUMN image, DROP reports_enabled;
+ALTER TABLE projects DROP COLUMN description;
+ALTER TABLE projects DROP COLUMN project_type_id;
+ALTER TABLE projects DROP COLUMN is_enabled;
+ALTER TABLE projects DROP COLUMN current_plan;
+ALTER TABLE projects DROP COLUMN storage_usage;
+ALTER TABLE projects DROP COLUMN processing_usage;
+ALTER TABLE projects DROP COLUMN pattern_matching_enabled;
+ALTER TABLE projects DROP COLUMN cnn_enabled;
+ALTER TABLE projects DROP COLUMN aed_enabled;
+ALTER TABLE projects DROP COLUMN clustering_enabled;
+ALTER TABLE projects DROP COLUMN featured;
+ALTER TABLE projects DROP COLUMN image;
+ALTER TABLE projects DROP COLUMN reports_enabled;
