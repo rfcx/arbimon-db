b97fb292e7d97a124967f9a13c33ec244821c7c4
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2024-11-08 11:24:10 +0400

    Update 042-add-validated-to-audio-event-detections-clustering sql

--- init/042-add-validated-to-audio-event-detections-clustering.sql
+++ init/042-add-validated-to-audio-event-detections-clustering.sql
@@ -3,3 +3,6 @@ ADD COLUMN `validated` tinyint(1) DEFAULT NULL;
 
 UPDATE `arbimon2`.`audio_event_detections_clustering` SET `validated` = 1
 WHERE `species_id` is not null and `songtype_id` is not null;
+
+ALTER TABLE `arbimon2`.`audio_event_detections_clustering`
+CHANGE cluster_number aed_number bigint(20);
