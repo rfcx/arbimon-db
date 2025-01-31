41e9cf0a729119431407bced8e78ae6132427df5
TooSeriuz <frongs@rfcx.org>
2025-01-31 22:29:09 +0700

    Fix job_params_retraining table

--- init/056-job-params-retraining-table.sql
+++ init/056-job-params-retraining-table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `job_params_retraining` (
   `job_id` bigint(20) unsigned NOT NULL,
-  `trained_model_id` int(10) unsigned DEFAULT NULL,
+  `trained_job_id` bigint(20) unsigned NOT NULL,
   PRIMARY KEY (`job_id`),
-  KEY `trained_model_id` (`trained_model_id`),
-  CONSTRAINT `job_params_training_ibfk_5` FOREIGN KEY (`trained_model_id`) REFERENCES `models` (`model_id`)
-);
+  KEY `trained_job_id` (`trained_job_id`),
+  CONSTRAINT `job_params_retraining_ibfk_1` FOREIGN KEY (`trained_job_id`) REFERENCES `jobs` (`job_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
