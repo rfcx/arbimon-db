3bf647836301366b7b5ffccbf4f16ec453ee312c
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2025-04-17 17:39:37 +0400

    #45 Update test cases for the training_sets table

--- init/058-add-source-project-id-to-training-sets.sql
+++ init/058-add-source-project-id-to-training-sets.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `arbimon2`.`training_sets`
-ADD COLUMN `source_project_id`  INT(10) unsigned DEFAULT NULL;
+ADD COLUMN `source_project_id` INT(10) unsigned DEFAULT NULL;
 
 CREATE INDEX training_sets_source_project_id ON training_sets(source_project_id);
