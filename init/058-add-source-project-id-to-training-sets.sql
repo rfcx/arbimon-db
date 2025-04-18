fb2c3fbc362145df0bafa53557e6c24dff540d8a
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2025-04-18 23:27:01 +0400

    #45 Remove index

--- init/058-add-source-project-id-to-training-sets.sql
+++ init/058-add-source-project-id-to-training-sets.sql
@@ -3,5 +3,3 @@ ADD COLUMN `source_project_id` INT(10) unsigned DEFAULT NULL;
 
 ALTER TABLE `arbimon2`.`training_sets`
 ADD CONSTRAINT `fk_source_project_id` FOREIGN KEY (`source_project_id`) REFERENCES `projects`(`project_id`);
-
-CREATE INDEX training_sets_source_project_id ON training_sets(source_project_id);
